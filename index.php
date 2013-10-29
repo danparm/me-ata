7ef1bda96d6f7a860ce96b9159cbbaea8f9b0405
Dan Parmelee <Dan@Daniels-MacBook-Pro.local>
2013-10-29 19:01:42 -0400

    adding files
    skeleton files

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Me-ata</title>
+
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+	<!-- jQuery -->
+	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+</head>
+<body>
+	<div class="header">
+		<div class="container">
+			<h1>Me-ata</h1>
+		</div>
+	</div>
+	<div class="main">
+		<div class="container">
+			<p>Lorem ipsum dolor sit amet</p>
+		</div>
+	</div>
+	<div class="footer">
+		<div class="container">
+
+		</div>
+	</div>
+</body>
+
+</html>
